63cb2ff2678302f2b5d9517a32883808256028bd
Andre-Faustino <andre.domingues.faustino@gmail.com>
2020-02-13 17:21:31 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,7 +28,15 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
-
+		
+		System.out.println("\n===TEST 3: seller findByAll ===");
+		
+		list = sellerDao.findAll();
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
 	}
 
 }
